0401cb41e58d4af4ca3feb0f83a65f536fb66c2a
Marie Hansteen <marie.hansteen@gmail.com>
2018-08-08 12:20:01 +0200

    Added a .php side with a name servey.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const index_php = require('/Users/mariehansteen/Lek/node-js-getting-started/views/pages/index_php.php')
 const cool = require('cool-ascii-faces')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -9,5 +10,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/index_php', (req, res) => res.send(index_php()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
