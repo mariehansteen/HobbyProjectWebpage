d27d7945c4ae2eeff77327bc7cd475fffd553f00
Marie Hansteen <marie.hansteen@gmail.com>
2018-08-12 09:37:35 +0200

    Attempt: Adding a path to a p5 game.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cool = require('cool-ascii-faces')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -11,3 +12,9 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+  .use(express.static(path.join(__dirname, 'p5')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('example/sketch'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
