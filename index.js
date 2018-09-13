06fe434f99d6aeaa4eb6c029be5761cb741879f2
Marie Hansteen <marie.hansteen@gmail.com>
2018-09-13 23:19:14 +0200

    Fikse forside farger, design etc.

--- index.js
+++ index.js
@@ -18,22 +18,3 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-//Users/mariehansteen/Documents/Lek/node-js-getting-started/views/p5/MasterMind
-
-// var app = express();
-// var server = app.listen(3000);
-// app.get('/p5_game',sayHello);
-
-// function sayHello(req,res){
-// 	res.send(app.use(express.static(path.join(__dirname, 'views/p5/example'))));
-// 	res.send("Hello! Nice to meet you");
-
-// }
-
-//express()
-  //.use(express.static(path.join(__dirname, 'public')))
-  //.set('views', path.join(__dirname, 'views'))
-  //.set('view engine', 'html')
-  //.get('/p5_game', (req, res) => res.render('p5/example/index'))
-
-  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
