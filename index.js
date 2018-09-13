383b02cc993ac2d8926c68a4787c788d5363b47d
Marie Hansteen <marie.hansteen@gmail.com>
2018-09-13 20:34:02 +0200

    MasterMind Button.. lets try again..

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  //.use(express.static(__dirname + '/views/p5')).set('views', path.join(__dirname, 'views'))
-  .use(express.static(__dirname + '/views/p5/MasterMind')).set('views', path.join(__dirname, 'views'))
+  .use(express.static(__dirname + '/views/pages')).set('views', path.join(__dirname, 'views'))
+  .use(express.static(__dirname + '/views/p5')).set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index.ejs'))
-  .get('/MasterMind', (req, res) => res.render('p5/MasterMind/sketch_MM3.js'))
+  .get('/MasterMind', (req, res) => res.send('p5/MasterMind/index.html'))
 
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
